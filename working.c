7733592b8209bbc03f3013357096d06c29e0aa70
Os-droid-hash <k250789@nu.edu.pk>
2025-09-15 10:36:07 +0500

    working.c

--- working.c
+++ working.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+	int main(){
+		char Degree[10];
+		char Interview[10];
+		printf("Enter Degree: ");
+		scanf("%s",Degree);
+		printf("Did you pass the interview");
+		
+		if (strcmp(Degree,"MS")==0){
+		    printf("Enter interview result: ");
+			scanf("%s",Interview);
+			if (strcmp(Interview,"Pass")==0){
+			    printf("You are hired");
+			}
+			else {
+			    printf("Not hired");
+			}
+		}
+		else {
+		    printf("Not Eligible");
+		}
+		
+}
